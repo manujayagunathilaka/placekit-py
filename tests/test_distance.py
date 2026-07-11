5d8de791cda44133d68a9b70fb58b304ade665bc
Manujaya Gunathilaka <168986608+manujayagunathilaka@users.noreply.github.com>
2026-07-11 14:45:34 +0530

    test: add distance calculator tests (#3)

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -12,3 +12,14 @@ def test_distance_between_same_coordinates():
     assert distance.km == 0
     assert distance.meters == 0
     assert distance.miles == 0
+
+
+def test_distance_between_two_coordinates():
+    distance = distance_between(
+        (6.9147, 79.9729),
+        (6.9271, 79.8612),
+    )
+
+    assert distance.km == 12.407
+    assert distance.meters == 12406.83
+    assert distance.miles == 7.709
