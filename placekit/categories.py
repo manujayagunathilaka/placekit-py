f6db9e248c9a0c888429cd220b6cf68a7bd16a9d
Manujaya Gunathilaka <168986608+manujayagunathilaka@users.noreply.github.com>
2026-07-15 05:56:52 +0530

    feat: add place category helper methods (#14)

--- placekit/categories.py
+++ placekit/categories.py
@@ -16,3 +16,21 @@ class PlaceCategory:
     BUS_STOP = "bus_stop"
     TRAIN_STATION = "train_station"
     HOTEL = "hotel"
+
+    @classmethod
+    def all(cls) -> list[str]:
+        """Return all supported place categories."""
+        return [
+            cls.UNIVERSITY,
+            cls.SCHOOL,
+            cls.HOSPITAL,
+            cls.PHARMACY,
+            cls.RESTAURANT,
+            cls.CAFE,
+            cls.SUPERMARKET,
+            cls.ATM,
+            cls.BANK,
+            cls.BUS_STOP,
+            cls.TRAIN_STATION,
+            cls.HOTEL,
+        ]
